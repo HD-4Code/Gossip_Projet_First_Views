79451d7a8ae52a4bc4a72318daf05ee946c14faf
HD-4Code <86689864+HD-4Code@users.noreply.github.com>
2021-08-03 04:36:29 +0200

    Add files via upload

---
+++ test/controllers/pages_controller_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class PagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get welcome" do
+    get pages_welcome_url
+    assert_response :success
+  end
+
+  test "should get team" do
+    get pages_team_url
+    assert_response :success
+  end
+
+  test "should get contact" do
+    get pages_contact_url
+    assert_response :success
+  end
+
+  test "should get gossip" do
+    get pages_gossip_url
+    assert_response :success
+  end
+
+end
